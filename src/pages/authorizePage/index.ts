c7af25b78469b5e592ad783daeaab396d91716e2
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2025-02-08 23:23:37 +0100

    Sprint 3

--- src/pages/authorizePage/index.ts
+++ src/pages/authorizePage/index.ts
@@ -78,26 +78,26 @@ export class AuthorizePage extends Block {
                 onClick: async (event: Event) => {
                     console.log('CLICK Submit button');
                     router.go(Urls.Chats);
-                //     const loginValue = (document.querySelector('#login-input') as HTMLInputElement).value;
-                //     const passwordValue = (document.querySelector('#password-input') as HTMLInputElement).value;
-                //     if (
-                //         (utils.validateLogin(loginValue)) &&
-                //         (utils.validatePassword(passwordValue))
-                //     ) {
-                //         console.log('Данные провалидированы');
-                //         console.log('Данные из формы:');
-                //         const form = document.querySelector('.login-form') as HTMLFormElement;
-                //         if (form) {
-                //             const formData = getFormData(form);
-                //             console.log(formData);
-                //             await AuthController.signin(formData as unknown as SignInData);
-                //         }
-                //     } else {
-                //         console.log('Необходимо правильно заполнить данные');
-                //     }
-                //     event.preventDefault();
-                //     event.stopPropagation();
-                // }
+                    // const loginValue = (document.querySelector('#login-input') as HTMLInputElement).value;
+                    // const passwordValue = (document.querySelector('#password-input') as HTMLInputElement).value;
+                    // if (
+                    //     (utils.validateLogin(loginValue)) &&
+                    //     (utils.validatePassword(passwordValue))
+                    // ) {
+                    //     console.log('Данные провалидированы');
+                    //     console.log('Данные из формы:');
+                    //     const form = document.querySelector('.login-form') as HTMLFormElement;
+                    //     if (form) {
+                    //         const formData = getFormData(form);
+                    //         console.log(formData);
+                    //         await AuthController.signin(formData as unknown as SignInData);
+                    //     }
+                    // } else {
+                    //     console.log('Необходимо правильно заполнить данные');
+                    // }
+                    // event.preventDefault();
+                    // event.stopPropagation();
+                }
             }),
             registerLink: new Link({
                 href: '/sign-up',
